4586a8dc0dcc5b7dac01770a613dd661ae56cf12
timur galiullin <timur.galiullin24@outlook.com>
2024-10-15 01:15:58 +0300

    Созданы классы для команд, базовый ViewModel и класс MainWindowViewModel

---
+++ ConstructionProjectManager.Client/Infrastructure/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using ConstructionProjectManager.Client.Infrastructure.Commands.Base;
+
+namespace ConstructionProjectManager.Client.Infrastructure
+{
+    internal class RelayCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            _execute(parameter);
+        }
+    }
+}
---
+++ ConstructionProjectManager.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ConstructionProjectManager.Client.Infrastructure;
+using ConstructionProjectManager.Client.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ConstructionProjectManager.Client.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Главная станица";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecute(object parameter) => true;
+        private void OnCloseApplicationCommandExecuted(object parameter) => Application.Current.Shutdown();
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+        }
+    }
+}
