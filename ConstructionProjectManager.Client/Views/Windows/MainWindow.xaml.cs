38184445d7f19b5be3e6598c1cc1befc1b12c0fb
timur galiullin <timur.galiullin24@outlook.com>
2024-10-15 01:09:53 +0300

    Создана начальная архитектура проекта

---
+++ ConstructionProjectManager.Client/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ConstructionProjectManager.Client
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
