6b2183d2ef30d7906428624f959d4ea606410d6b
timur galiullin <timur.galiullin24@outlook.com>
2024-10-15 05:45:27 +0300

    Внесены изменения в MainWindow

--- ConstructionProjectManager.Client/ViewModels/MainWindowViewModel.cs
+++ ConstructionProjectManager.Client/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,44 @@
-﻿using ConstructionProjectManager.Client.Infrastructure.Commands;
+﻿using ConstructionProjectManager.Client.Models;
 using ConstructionProjectManager.Client.ViewModels.Base;
-using System.Windows;
-using System.Windows.Input;
+using System.Collections.ObjectModel;
 
 namespace ConstructionProjectManager.Client.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        private string _title = "Главная станица";
+        public ObservableCollection<User> Users { get; }
 
+        private User _selectedUser;
+        public User SelectedUser
+        {
+            get => _selectedUser;
+            set => Set(ref _selectedUser, value);
+        }
+
+        private string _title = "Главная станица";
         public string Title
         {
             get => _title;
             set => Set(ref _title, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            int project_index = 1;
+            var projects = Enumerable.Range(1, 10).Select(i => new Project
+            {
+                Name = $"Проект {project_index++}",
+                CreatedDate = DateTime.Now,
+                CompletedDate = DateTime.Now + TimeSpan.FromDays(20),
+            });
+
+            var users = Enumerable.Range(1, 20).Select(i => new User
+            {
+                Username = $"Сотрудник {i}",
+                Projects = new ObservableCollection<Project>(projects)
+            });
+
+            Users = new ObservableCollection<User>(users);
         }
     }
 }
